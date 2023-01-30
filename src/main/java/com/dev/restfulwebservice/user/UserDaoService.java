e3574cfbf8a75ab099e402ec5606e2acabd2fb3a
freddykembo <freddykembo@gmail.com>
2023-01-30 23:32:54 +0300

    Added user create endpoint and dao service method

--- src/main/java/com/dev/restfulwebservice/user/UserDaoService.java
+++ src/main/java/com/dev/restfulwebservice/user/UserDaoService.java
@@ -13,10 +13,12 @@ public class UserDaoService {
 
     private static List<User> users = new ArrayList<>();
 
+    private static int usersCount = 0;
+
     static {
-        users.add(new User(1,"Fred",LocalDate.now().minusYears(35)));
-        users.add(new User(2,"Kembo",LocalDate.now().minusYears(30)));
-        users.add(new User(3,"Otis",LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount,"Fred",LocalDate.now().minusYears(35)));
+        users.add(new User(++usersCount,"Kembo",LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount,"Otis",LocalDate.now().minusYears(25)));
     }
 
     public List<User> findAll() {
@@ -26,6 +28,12 @@ public class UserDaoService {
     public User findOne(int id) {
         Predicate<? super User> predicate = user -> user.getId().equals(id);
         return users.stream().filter(predicate).findFirst().get();
+    }
+
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
     }
 
 }
