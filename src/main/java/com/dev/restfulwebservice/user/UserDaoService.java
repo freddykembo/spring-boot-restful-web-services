484118c1f7dc9b5459569d12e87fe4065b4fd794
freddykembo <freddykembo@gmail.com>
2023-01-30 23:13:58 +0300

    Added endpoint to list users/user

--- src/main/java/com/dev/restfulwebservice/user/UserDaoService.java
+++ src/main/java/com/dev/restfulwebservice/user/UserDaoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
@@ -14,12 +15,17 @@ public class UserDaoService {
 
     static {
         users.add(new User(1,"Fred",LocalDate.now().minusYears(35)));
-        users.add(new User(1,"Kembo",LocalDate.now().minusYears(30)));
-        users.add(new User(1,"Otis",LocalDate.now().minusYears(25)));
+        users.add(new User(2,"Kembo",LocalDate.now().minusYears(30)));
+        users.add(new User(3,"Otis",LocalDate.now().minusYears(25)));
     }
 
     public List<User> findAll() {
         return users;
+    }
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
     }
 
 }
