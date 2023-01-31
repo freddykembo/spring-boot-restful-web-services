7bdc6a29db00178397f0a7790c28f524c6ee4ade
freddykembo <freddykembo@gmail.com>
2023-01-31 18:49:12 +0300

    Delete endpoint added

--- src/main/java/com/dev/restfulwebservice/user/UserResource.java
+++ src/main/java/com/dev/restfulwebservice/user/UserResource.java
@@ -42,4 +42,9 @@ public class UserResource {
 
         return ResponseEntity.created(location).build();
     }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        service.deleteById(id);
+    }
 }
