68c09eb21cbc71f1d50345dc89ba42ebdd9a19dd
freddykembo <freddykembo@gmail.com>
2023-01-31 23:26:46 +0300

    Validation and customized error message for name and birt date added

--- src/main/java/com/dev/restfulwebservice/user/User.java
+++ src/main/java/com/dev/restfulwebservice/user/User.java
@@ -1,10 +1,17 @@
 package com.dev.restfulwebservice.user;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class User {
     private Integer id;
+
+    @Size(min=2, message = "name should have at least 2 characters")
     private String name;
+
+    @Past(message = "Birt date should be in the past")
     private LocalDate birtDate;
 
     public User(Integer id, String name, LocalDate birtDate) {
